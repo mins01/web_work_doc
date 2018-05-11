3140669156b189fb873314a32da58999bdb49f98
공대여자 <mins01.lycos.co.kr@gmail.com>
2018-05-11 18:16:25 +0900

    file_get_contents -> mproxy

--- weather/proxy.php
+++ weather/proxy.php
@@ -2,7 +2,13 @@
 $url = 'http://www.weather.go.kr/wid/queryDFSRSS.jsp?zone='.$_GET['zone'];
 
 require('php/XML2Array.php');
-$xml = file_get_contents($url);
+require('php/Mproxy.php');
+$mp = new Mproxy();
+$res = $mp->getContent($url);
+$xml = $res['body'];
+// $xml = file_get_contents($url);
+
+
 $arr = XML2Array::createArray($xml);
 header('Content-Type: application/json');
 if(defined('JSON_UNESCAPED_UNICODE')){
