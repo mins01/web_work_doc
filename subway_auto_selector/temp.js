02e5fa5299b8a2051ef570ebb0a945791ec7cff3
공대여자 <mins01.lycos.co.kr@gmail.com>
2018-06-14 17:06:59 +0900

    목록화

--- subway_auto_selector/temp.js
+++ subway_auto_selector/temp.js
@@ -5,18 +5,18 @@ var retfn = function(){
 var r = []
 // r.push('class\ttit\teng\tcal\tsummary');
 $('.pd_list_wrapper >ul>li').each(
-    function(idx,el){
-        var cat = document.location.pathname.replace('/','');
-        var tit = $(this).find(".tit").map(retfn).get();
-        var eng = $(this).find(".eng").map(retfn).get();
-        var cal = $(this).find(".cal").map(retfn).get();
-        var summary = $(this).find(".summary").map(retfn).get();
-        var img = $(this).find(".img img").map(retfn).attr('src');
-        console.log(img);
-        for(var i=0,m=tit.length;i<m;i++){
-            r.push(['cat',this.className,tit[i],eng[i],(cal[i]?cal[i]:''),(summary[i]?summary[i]:''),(img[i]?img[i].replace('../','http://subway.co.kr/'):'')]);
-        }
-    }
+	function(idx,el){
+		var cat = document.location.pathname.replace('/','');
+		var tit = $(this).find(".tit").map(retfn).get();
+		var eng = $(this).find(".eng").map(retfn).get();
+		var cal = $(this).find(".cal").map(retfn).get();
+		var summary = $(this).find(".summary").map(retfn).get();
+		var img = $(this).find(".img img").map(function(){return this.src.replace('../','http://subway.co.kr/')});
+		// console.log(img);
+		for(var i=0,m=tit.length;i<m;i++){
+			r.push([cat,this.className,tit[i],eng[i],(cal[i]?cal[i]:''),(summary[i]?summary[i]:''),(img[i]?img[i]:'')]);
+		}
+	}
 )
 
 
