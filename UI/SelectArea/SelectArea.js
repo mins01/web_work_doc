36e5bf2a7f56ea9c7f9d6cae01fd59b113eeb492
공대여자 <mins01.lycos.co.kr@gmail.com>
2018-11-14 10:01:50 +0900

    이미지 리사이즈도 포함

--- UI/SelectArea/SelectArea.js
+++ UI/SelectArea/SelectArea.js
@@ -1,183 +1,22 @@
-r SelectArea = {
-	"sa":null,
-	"box":null,
-	"bg":null,
-	"parent":null,
-	"x":null,"y":null,"w":null,"h":null,
-	"init":function(){
-		
-	},
-	"getBoundingClientRect":function(){
-		if(!this.sa.parentNode){
-			return null;
-		}
-		return this.box.layout.getBoundingClientRect();
-	},
-	"_create":function(){
-		if(this.sa != null){return this.sa;}
-		this.sa = document.createElement('div');
-		this.sa.className="selectArea";
-		this.sa.innerHTML =
-		'<div class="selectArea-box">'+
-		'<div class="selectArea-pointer" data-index="0" data-x="0" data-y="0"></div>'+
-		'<div class="selectArea-pointer" data-index="1"></div>'+
-		'<div class="selectArea-pointer" data-index="2"></div>'+
-		'<div class="selectArea-pointer" data-index="3"></div>'+
-		'<div class="selectArea-pointer" data-index="4" data-w="0" data-h="0"></div>'+
-		'<div class="selectArea-pointer" data-index="5"></div>'+
-		'<div class="selectArea-pointer" data-index="6"></div>'+
-		'<div class="selectArea-pointer" data-index="7"></div>'+
-		'<div class="selectArea-layout"></div>'+
-		'</div>'+
-		'<div class="selectArea-bg"></div>';
-		this.box=this.sa.querySelector('.selectArea-box');
-		this.bg=this.sa.querySelector('.selectArea-bg');
-		this.box.pointers=this.box.querySelectorAll('.selectArea-pointer');
-		this.box.layout=this.box.querySelector('.selectArea-layout');
-		this.box.layout.ondblclick = this._ondblclick();
-		this.toDragable.addListener(this.box.layout,function(thisC){return function(evt,gapX,gapY){
-			thisC.syncPosBy(gapX,gapY,0,0);
-		}}(this),this._onchange());
-		this.toDragable.addListener(this.box.pointers[0],function(thisC){return function(evt,gapX,gapY){
-			thisC.syncPosBy(gapX,gapY,-1*gapX,-1*gapY);
-		}}(this),this._onchange());
-		this.toDragable.addListener(this.box.pointers[1],function(thisC){return function(evt,gapX,gapY){
-			thisC.syncPosBy(0,gapY,0,-1*gapY);
-		}}(this),this._onchange());
-		this.toDragable.addListener(this.box.pointers[2],function(thisC){return function(evt,gapX,gapY){
-			thisC.syncPosBy(0,gapY,gapX,-1*gapY);
-		}}(this),this._onchange());
-		this.toDragable.addListener(this.box.pointers[3],function(thisC){return function(evt,gapX,gapY){
-			thisC.syncPosBy(0,0,gapX,0);
-		}}(this),this._onchange());
-		this.toDragable.addListener(this.box.pointers[4],function(thisC){return function(evt,gapX,gapY){
-			thisC.syncPosBy(0,0,gapX,gapY);
-		}}(this),this._onchange());
-		this.toDragable.addListener(this.box.pointers[5],function(thisC){return function(evt,gapX,gapY){
-			thisC.syncPosBy(0,0,0,gapY);
-		}}(this),this._onchange());
-		this.toDragable.addListener(this.box.pointers[6],function(thisC){return function(evt,gapX,gapY){
-			thisC.syncPosBy(gapX,0,-1*gapX,gapY);
-		}}(this),this._onchange());
-		this.toDragable.addListener(this.box.pointers[7],function(thisC){return function(gapX,gapY){
-			thisC.syncPosBy(gapX,0,-1*gapX,0);
-		}}(this),this._onchange());
-		return this.sa
-	},
-	"__onchange":null,
-	"_onchange":function(){
-		var thisC = this
-		return function(evt){
-			if(thisC.__onchange){
-				thisC.__onchange(evt);
-			}
-		}
-	},
-	"__ondblclick":null,
-	"_ondblclick":function(){
-		var thisC = this
-		return function(evt){
-			if(thisC.__ondblclick){
-				thisC.__ondblclick(evt);
-			}
-		}
-	},
-	"hide":function(){
-		if(this.sa.parentNode){
-			this.sa.parentNode.removeChild(this.sa);
-		}
-	},
-	"show":function(parent,opt){
-		this._create();
-		document.body.appendChild(this.sa);
-		this.parent = parent;
-		var p_bcr = this.parent.getBoundingClientRect();
-		this.w = p_bcr.width;
-		this.h = p_bcr.height;
-		var x,y,w,h;
-		if(opt){
-			x = opt.x;
-			y = opt.y;
-			w = opt.w;
-			h = opt.h;
-		}
-		this.__onchange = opt&&opt.onchange?opt.onchange:null;
-		this.__ondblclick = opt&&opt.ondblclick?opt.ondblclick:null;
-		if(x==undefined){x=0;}
-		if(y==undefined){y=0;}
-		if(w==undefined){w=p_bcr.width;}
-		if(h==undefined){h=p_bcr.height;}
-		this._syncPos(x,y,w,h);
-		window.addEventListener('resize',function(thisC){return function(evt){thisC._onresize(evt);} }(this),false)
-		window.addEventListener('scroll',function(thisC){return function(evt){thisC._onresize(evt);} }(this),false)
-		
-	},
-	"_onresize":function(evt){
-		this._syncPos(this.x,this.y,this.w,this.h);
-		evt.preventDefault();evt.stopPropagation();
-		if(this.__onchange) this.__onchange()
-	},
-	"syncPosBy":function(x,y,w,h){
-		
-		if(this.w==0){x=0;}
-		if(this.h==0){y=0;}
-		if(x!=0 && this.x+x<0){
-			x=0;
-			if(w!=0) w = 0;
-		}
-		if(y!=0 && this.y+y<0){
-			y=0;
-			if(h!=0) h = 0;
-		}
-		this._syncPos(this.x+x,this.y+y,this.w+w,this.h+h);
-	},
-	"syncPosTo":function(x,y,w,h){
-		if(x==null) x = this.x;
-		if(y==null) y = this.y;
-		if(w==null) w = this.w;
-		if(h==null) h = this.h;
-		this._syncPos(x,y,w,h);
-	},	
-	"_syncPos":function(x,y,w,h){
-		var t ;
-		var scrollX = (((t = document.documentElement) || (t = document.body.parentNode)) && typeof t.scrollLeft == 'number' ? t : document.body).scrollLeft;
-		var scrollY = (((t = document.documentElement) || (t = document.body.parentNode))  && typeof t.scrollTop == 'number' ? t : document.body).scrollTop
-		var p_bcr = this.parent.getBoundingClientRect();
-		
-		x = Math.min(Math.max(0,x),p_bcr.width-w);
-		y = Math.min(Math.max(0,y),p_bcr.height-h);
-		w = Math.min(Math.max(0,w),p_bcr.width-x);
-		h = Math.min(Math.max(0,h),p_bcr.height-y);
-		x = Math.min(Math.max(0,x),p_bcr.width-w);
-		y = Math.min(Math.max(0,y),p_bcr.height-h);
-		x = Math.max(0,x);
-		y = Math.max(0,y);
-		w = Math.min(w,p_bcr.width);
-		h = Math.min(h,p_bcr.height);
+(function () {
 
-		this.x = x;
-		this.y = y;
-		this.w = w;
-		this.h = h;
-		this.box.pointers[0].setAttribute('data-x',x.toFixed(0));
-		this.box.pointers[0].setAttribute('data-y',y.toFixed(0));
-		this.box.pointers[4].setAttribute('data-w',w.toFixed(0));
-		this.box.pointers[4].setAttribute('data-h',h.toFixed(0));
-		
-		this.box.style.top = y+"px";
-		this.box.style.left = x+"px";
-		this.box.style.width = w+"px";
-		this.box.style.height = h+"px";
-		this.sa.style.top = p_bcr.top+scrollY+"px";
-		this.sa.style.left = p_bcr.left+scrollX+"px";
-		this.sa.style.width = p_bcr.width+"px";
-		this.sa.style.height = p_bcr.height+"px";
-		this.bg.style.borderTopWidth = y+"px";
-		this.bg.style.borderLeftWidth = x+"px";
-		this.bg.style.borderRightWidth = (p_bcr.width-x-w)+"px";
-		this.bg.style.borderBottomWidth =  (p_bcr.height-y-h)+"px";
-	}
-	,"toDragable":{
+  if ( typeof window.CustomEvent === "function" ) return false;
+
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+   }
+
+  CustomEvent.prototype = window.Event.prototype;
+
+  window.CustomEvent = CustomEvent;
+})();
+
+var SelectArea = (function(){
+	
+	var toDragable = {
 		"addListener":function(target,cb_onpointerMove,cb_onpointerUp){
 			target.ing = false;
 			target.x0 = 0;
@@ -185,7 +24,7 @@ r SelectArea = {
 			target.cb_onpointerMove = cb_onpointerMove;
 			target.cb_onpointerUp = cb_onpointerUp;
 			target._getXY = this._getXY;
-
+			
 			target.addEventListener('pointerdown',this._onpointerdown(target));
 			target.addEventListener('touchmove',function(evt){ evt.preventDefault();evt.stopPropagation()	;return false;});
 			document.addEventListener('pointermove',this._onpointermove(target));
@@ -230,20 +69,19 @@ r SelectArea = {
 				target.x0 = xy[0];
 				target.y0 = xy[1];
 				if(target.cb_onpointerMove){
-					target.cb_onpointerMove(evt,gapX,gapY); 	
+					target.cb_onpointerMove(evt,gapX,gapY);
 				}
 				evt.preventDefault();evt.stopPropagation()	;
 				return false;
 			}
-		}
-		,
+		},
 		"_onpointerup":function(target){
 			return function(evt){
 				// var target = evt.target;
-				target.ing = false;
-				if(target.cb_onpointerUp){
-					target.cb_onpointerUp(evt); 	
+				if(target.ing && target.cb_onpointerUp){
+					target.cb_onpointerUp(evt);
 				}
+				target.ing = false;
 				evt.preventDefault();evt.stopPropagation()	;
 			}
 			
@@ -251,4 +89,219 @@ r SelectArea = {
 	}
 	
 	
-}
+	var _create = function(target){
+		var sa = document.createElement('div');
+		sa.className="selectArea";
+		sa.innerHTML =
+		'<div class="selectArea-box">'+
+		'<div class="selectArea-pointer" data-index="0" data-x="0" data-y="0"></div>'+
+		'<div class="selectArea-pointer" data-index="1"></div>'+
+		'<div class="selectArea-pointer" data-index="2"></div>'+
+		'<div class="selectArea-pointer" data-index="3"></div>'+
+		'<div class="selectArea-pointer" data-index="4" data-w="0" data-h="0"></div>'+
+		'<div class="selectArea-pointer" data-index="5"></div>'+
+		'<div class="selectArea-pointer" data-index="6"></div>'+
+		'<div class="selectArea-pointer" data-index="7"></div>'+
+		'<div class="selectArea-layout"></div>'+
+		'</div>'+
+		'<div class="selectArea-bg"></div>';
+		sa.box=sa.querySelector('.selectArea-box');
+		sa.bg=sa.querySelector('.selectArea-bg');
+		sa.box.pointers=sa.querySelectorAll('.selectArea-pointer');
+		sa.box.layout=sa.querySelector('.selectArea-layout');
+    sa.selectedArea = sa.box.layout;
+		sa.target = target
+		sa.target.sa = sa;
+		var p_bcr = sa.target.getBoundingClientRect();
+
+		sa.x = 0;
+		sa.y = 0;
+		sa.w = p_bcr.width;
+		sa.h = p_bcr.height;
+		sa.x1 = sa.x+sa.w;
+		sa.y1 = sa.y+sa.h;
+		return sa;
+	}
+	var _initMethod = function(sa){
+		sa.show = function(x,y,x1,y1){
+			document.body.appendChild(this);
+			var p_bcr = this.target.getBoundingClientRect();
+			// console.log(p_bcr)
+			if(x == undefined) x = 0;
+			if(y == undefined) y = 0;
+			if(x1 == undefined) x1 = p_bcr.width + x;
+			if(y1 == undefined) y1 = p_bcr.height + y;
+			var t = 0;
+			if(x > x1){t = x;x = x1;x1 = t;}
+			if(y > y1){t = y;y = y1;y1 = t;}
+			this.x = x;
+			this.y = y;
+			this.x1 = x1;
+			this.y1 = y1
+			this.syncPosCoordinate(x,y,x1,y1);
+		}
+		sa.hide = function(){
+			if(this.parentNode) this.parentNode.removeChild(this);
+		}
+		sa.destroy = function(){
+      window.removeEventListener('resize',this._window_onresize);
+      window.removeEventListener('scroll',this._window_onresize);
+			this.parentNode.removeChild(this);
+      this.show = null;
+			delete this.target.sa;
+		}
+		sa.syncPosCoordinate = function(x,y,x1,y1){
+			_syncPosCoordinate(this,x,y,x1,y1);
+		}
+		sa.syncPos = function(x,y,w,h){
+			var p_bcr = sa.target.getBoundingClientRect();
+			if(x+w>p_bcr.width){
+				x=p_bcr.width-this.w;
+			}
+			if(y+h>p_bcr.height){
+				y=p_bcr.height-this.h;
+			}
+			this.syncPosCoordinate(x,y,x+w,y+h);
+		}
+		sa.syncPosBy = function(x,y,w,h){
+			if(this.w==0){x=0;}
+			if(this.h==0){y=0;}
+			var p_bcr = sa.target.getBoundingClientRect();
+			if(x!=0 && (this.x1+x>p_bcr.width || this.x+x<0)){
+				x=0;
+				w=0;
+			}
+			if(y!=0 && (this.y1+y>p_bcr.height || this.y+y<0)){
+				y=0;
+				h=0;
+			}
+
+			this.syncPos(this.x+x,this.y+y,this.w+w,this.h+h);
+		}
+    sa.resync = function(){
+      this.syncPosCoordinate(this.x,this.y,this.x1,this.y1);
+    }
+		sa.syncPosCoordinateBy = function(x,y,x1,y1){
+			var t = 0;
+			// var p_bcr = sa.target.getBoundingClientRect();
+
+			var x_e = this.x+x;
+			var y_e = this.y+y;
+			var x1_e = this.x1+x1;
+			var y1_e = this.y1+y1;
+			var w = x1_e-x_e;
+			var h = y1_e-y_e;
+			// console.log(x,y,w,h,x1,y1)
+			this.syncPosCoordinate(x_e,y_e,x1_e,y1_e);
+		}
+		sa._window_onresize = function(thisC){
+      return function(evt){
+  			thisC.resync();
+  			evt.preventDefault();evt.stopPropagation();
+      }
+		}(sa)
+		sa.toDragable_onpointerup = function(thisC){
+			return function(evt){
+				var t = 0;
+				if(thisC.x > thisC.x1){t = thisC.x;thisC.x = thisC.x1;thisC.x1 = t;}
+				if(thisC.y > thisC.y1){t = thisC.y;thisC.y = thisC.y1;thisC.y1 = t;}
+				thisC.syncPosCoordinate(thisC.x,thisC.y,thisC.x1,thisC.y1);
+			}
+		}(sa)
+		
+		
+	}
+	var _initEvent = function(sa){
+		toDragable.addListener(sa.box.layout,function(thisC){return function(evt,gapX,gapY){
+			thisC.syncPosBy(gapX,gapY,0,0);
+			thisC.dispatchEvent((new CustomEvent("move", {}) ));
+		}}(sa),sa.toDragable_onpointerup);
+		toDragable.addListener(sa.box.pointers[0],function(thisC){return function(evt,gapX,gapY){
+			thisC.syncPosCoordinateBy(gapX,gapY,0,0)
+			thisC.dispatchEvent((new CustomEvent("move", {})));
+		}}(sa),sa.toDragable_onpointerup);
+		toDragable.addListener(sa.box.pointers[1],function(thisC){return function(evt,gapX,gapY){
+			thisC.syncPosCoordinateBy(0,gapY,0,0)
+			thisC.dispatchEvent((new CustomEvent("move", {})));
+		}}(sa),sa.toDragable_onpointerup);
+		toDragable.addListener(sa.box.pointers[2],function(thisC){return function(evt,gapX,gapY){
+			thisC.syncPosCoordinateBy(0,gapY,gapX,0)
+			thisC.dispatchEvent((new CustomEvent("move", {})));
+		}}(sa),sa.toDragable_onpointerup);
+		toDragable.addListener(sa.box.pointers[3],function(thisC){return function(evt,gapX,gapY){
+			thisC.syncPosCoordinateBy(0,0,gapX,0)
+			thisC.dispatchEvent((new CustomEvent("move", {})));
+		}}(sa),sa.toDragable_onpointerup);
+		toDragable.addListener(sa.box.pointers[4],function(thisC){return function(evt,gapX,gapY){
+			thisC.syncPosCoordinateBy(0,0,gapX,gapY)
+			thisC.dispatchEvent((new CustomEvent("move", {})));
+		}}(sa),sa.toDragable_onpointerup);
+		toDragable.addListener(sa.box.pointers[5],function(thisC){return function(evt,gapX,gapY){
+			thisC.syncPosCoordinateBy(0,0,0,gapY)
+			thisC.dispatchEvent((new CustomEvent("move", {})));
+		}}(sa),sa.toDragable_onpointerup);
+		toDragable.addListener(sa.box.pointers[6],function(thisC){return function(evt,gapX,gapY){
+			thisC.syncPosCoordinateBy(gapX,0,0,gapY)
+			thisC.dispatchEvent((new CustomEvent("move", {})));
+		}}(sa),sa.toDragable_onpointerup);
+		toDragable.addListener(sa.box.pointers[7],function(thisC){return function(evt,gapX,gapY){
+			thisC.syncPosCoordinateBy(gapX,0,0,0)
+			thisC.dispatchEvent((new CustomEvent("move", {})));
+		}}(sa),sa.toDragable_onpointerup);
+    
+    window.addEventListener('resize',sa._window_onresize,false)
+    window.addEventListener('scroll',sa._window_onresize,false)
+	}
+
+	var _syncPosCoordinate = function(sa,x,y,x1,y1){
+		var t ;
+		var scrollX = (((t = document.documentElement) || (t = document.body.parentNode)) && typeof t.scrollLeft == 'number' ? t : document.body).scrollLeft;
+		var scrollY = (((t = document.documentElement) || (t = document.body.parentNode))  && typeof t.scrollTop == 'number' ? t : document.body).scrollTop
+		var p_bcr = sa.target.getBoundingClientRect();
+
+		var x_e = Math.max(x,0);
+		var x1_e = Math.min(x1,p_bcr.width);
+		var y_e = Math.max(y,0);
+		var y1_e = Math.min(y1,p_bcr.height);
+		
+
+		var x_min = Math.max(Math.min(x_e,x1_e),0);
+		var y_min = Math.max(Math.min(y_e,y1_e),0);
+		var x_max = Math.min(Math.max(x_e,x1_e),p_bcr.width);
+		var y_max = Math.min(Math.max(y_e,y1_e),p_bcr.width);
+		var w = Math.abs(x_max-x_min);
+		var h = Math.abs(y_max-y_min);
+		sa.x = x_e;
+		sa.y = y_e;
+		sa.w = w;
+		sa.h = h;
+		sa.x1 = x1_e;
+		sa.y1 = y1_e;
+
+		sa.box.pointers[0].setAttribute('data-x',x_min.toFixed(0));
+		sa.box.pointers[0].setAttribute('data-y',y_min.toFixed(0));
+		sa.box.pointers[4].setAttribute('data-w',w.toFixed(0));
+		sa.box.pointers[4].setAttribute('data-h',h.toFixed(0));
+		
+		sa.box.style.top = y_min+"px";
+		sa.box.style.left = x_min+"px";
+		sa.box.style.width = w+"px";
+		sa.box.style.height = h+"px";
+		sa.style.top = p_bcr.top+scrollY+"px";
+		sa.style.left = p_bcr.left+scrollX+"px";
+		sa.style.width = p_bcr.width+"px";
+		sa.style.height = p_bcr.height+"px";
+		sa.bg.style.borderTopWidth = y_min+"px";
+		sa.bg.style.borderLeftWidth = x_min+"px";
+		sa.bg.style.borderRightWidth = (p_bcr.width-x_min-w)+"px";
+		sa.bg.style.borderBottomWidth =  (p_bcr.height-y_min-h)+"px";
+	}
+	
+	return function(target){
+		var sa = _create(target);
+		_initMethod(sa);
+		_initEvent(sa)
+		return sa;
+		
+	}
+})()
