711f9c5224dc8ddda066478caa6fa1918b4b8aa5
공대여자 <mins01.lycos.co.kr@gmail.com>
2020-10-09 00:08:24 +0900

    AUTOPLAY 추가 확률 조정 및 배율 조정

--- CSS3/preserve-3d/slotmachine.js
+++ CSS3/preserve-3d/slotmachine.js
@@ -1,6 +1,7 @@
 var slotmachine = (function(){
 	var slotmachine = {
 		playing:false,
+		autoplay:false,
 		runCount:0,
 		coin:0,
 		batCoin:0,
@@ -69,49 +70,57 @@ var slotmachine = (function(){
 			var r = [rs[0],rs[1],rs[2]];
 			r.sort();
 			console.log(r);
-			if(r[0]==7 && r[1]==7 && r[2]==7){ // 777
-				return 30;
-			}else if(r[0]==r[1] && r[2]==r[1]){ // 전부 같은 숫자
-				return 20;
-			}else if(r[1]==r[0]+1 && r[1]==r[2]-1){ //연속된 숫자
-				return 10;
-			}else if(r[0]==r[1] || r[0]==r[2] || r[1]==r[2] ){ // 두개가 같은 숫자
-				return 5;
-			}else if(r[0]%3==0 && r[1]%3==0 && r[2]%3==0 ){ // 3의 배수
-				return 3;
-			}else if(r[0]%2==0 && r[1]%2==0 && r[2]%2==0 ){ //모두 짝수
-				return 2;
-			}else if(r[0]%2==1 && r[1]%2==1 && r[2]%2==1 ){ //모두 홀수
+			if(rs[0]==1 && rs[1]==1 && rs[2]==9){ // 119. 확률 1/(8*8*8) = 1/512 = 0.001953125
+				return 119;
+			}else if(r[0]==7 && r[1]==7 && r[2]==7){ // 777. 확률 1/(8*8*8) = 1/512 = 0.001953125
+				return 100;
+			}else if(rs[0]+1 == rs[1] && rs[1]+1==rs[2]){ //순서 연속된 숫자. 확률 6/8 * 1/8 * 1/8 = 6/512 = 0.01171875
+				return 16;
+			}else if(r[0]==r[1] && r[2]==r[1]){ // 전부 같은 숫자. 확률  1/64 = 0.015625
+				return 8;
+			}
+			// else if(r[0]%3==0 && r[1]%3==0 && r[2]%3==0 ){ // 3의 배수. 확률 1/(4*4*4) = 1/64 = 0.015625
+			// 	return 8;
+			// }
+			else if(r[1]==r[0]+1 && r[1]==r[2]-1){ //순서무시 연속된 숫자. 확률 1/(4*4) = 1/16 = 0.0625
+				return 4;
+			}else if(r[0]==r[1] || r[0]==r[2] || r[1]==r[2] ){ // 두개가 같은 숫자. 확률 1/8 = 0.125
 				return 2;
 			}
+			// else if(r[0]%2==0 && r[1]%2==0 && r[2]%2==0 ){ //모두 짝수. 확률 1/(2*2*2) = 1/8 = 0.125
+			// 	return 2;
+			// }else if(r[0]%2==1 && r[1]%2==1 && r[2]%2==1 ){ //모두 홀수. 확률 1/(2*2*2) = 1/8 = 0.125
+			// 	return 2;
+			// }
 			return 0;
 		},
 		pullLever:function(){
 			if(this.playing){
-				alert("이미 게임 중입니다.");
+				// alert("이미 게임 중입니다.");
 				return;
 			}
 			if(this.coin == 0 ){
-				alert("코인을 넣어주세요.");
+				if(!this.autoplay) alert("코인을 넣어주세요.");
 				return;
 			}
 			if(this.batCoin == 0 ){
-				alert("코인을 배팅해주세요.");
+				if(!this.autoplay) alert("코인을 배팅해주세요.");
 				return;
 			}
 			if(this.batCoin > this.coin){
-				alert("보유 코인보다 배팅 코인이 더 많습니다.");
+				if(!this.autoplay) alert("보유 코인보다 배팅 코인이 더 많습니다.");
 				return;
 			}
 			this.playing = true;
 			this.coin -= this.batCoin;
+			this.runCount++;
 			slotmachine.rollSlot(0,Math.floor(Math.random()*8)+1);
 			setTimeout(function(){
 				slotmachine.rollSlot(1,Math.floor(Math.random()*8)+1);
-			},250);
+			},500);
 			setTimeout(function(){
 				slotmachine.rollSlot(2,Math.floor(Math.random()*8)+1);
-			},500);
+			},1000);
 			this.sync();
 		},
 		processResult:function(rs){
@@ -124,8 +133,12 @@ var slotmachine = (function(){
 			this.lastResultCoin = this.batCoin * this.lastMultiply;
 			this.coin += this.lastResultCoin;
 			this.playing = false;
-			this.runCount++;
 			this.sync();
+			if(this.autoplay){
+				setTimeout(function(){
+					if(slotmachine.autoplay) slotmachine.pullLever()
+				},1000)
+			}
 
 		},
 		insertCoin:function(coin){
@@ -134,6 +147,10 @@ var slotmachine = (function(){
 			this.sync();
 		},
 		setBatCoin:function(coin){
+			if(this.playing){
+				// alert("이미 게임 중입니다.");
+				return;
+			}
 			coin = parseInt(coin,10)
 			if(coin>this.coin){
 				alert("보유 코인보다 배팅 코인이 더 많습니다.");
@@ -145,6 +162,7 @@ var slotmachine = (function(){
 		sync:function(){
 			document.querySelector("#coin").innerText = this.coin;
 			document.querySelector("#batCoin").innerText = this.batCoin;
+			document.querySelector("#runCount").innerText = this.runCount+"회";
 			if(this.playing || this.runCount==0){
 				document.querySelector("#lastResult").innerText = this.batCoin+" X ?? = ???";
 			}else{
