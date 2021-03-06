7abaab3d6a1351c69a7a37b8e1ea44719361a7a0
공대여자 <mins01.lycos.co.kr@gmail.com>
2021-03-06 22:55:02 +0900

    speechSynthesis end

--- HTML5/speechSynthesis/app.js
+++ HTML5/speechSynthesis/app.js
@@ -4,10 +4,12 @@ function gerenate_speechSynthesis_app(el){
 		data: {
 			"enable":false,
 			"voices":[],
+			"speaking":false,
 		}
 	});
 	app.enable = !!window.speechSynthesis;
 	if(app.enable){
+		app.speechSynthesis = window.speechSynthesis
 		app.voices = window.speechSynthesis.getVoices();
 		if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
 			speechSynthesis.onvoiceschanged = function(){
