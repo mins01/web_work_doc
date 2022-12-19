383594c009ac63af2aaad23d8c37766bae6f4fdd
mins01 <mins01.lycos.co.kr@gmail.com>
2022-12-19 11:29:32 +0900

    프로파일가져오기 초벌 완료

--- crawler/instagram/CrawlerInstagram.php
+++ crawler/instagram/CrawlerInstagram.php
@@ -52,31 +52,82 @@ class CrawlerInstagram{
         //--- 기본 데이터
         $cacheKey = "_{$username}.html";
         $url = "https://www.instagram.com/{$username}/";
-        $res = $this->curlGetResWithCache($url,$cacheKey,86400);
+        $res = $this->curlGetResWithCache($url,array(),$cacheKey,86400);
         // $res['body'] = '';
         $matches = [];
         preg_match_all('/{\\\"csrf_token\\\":\\\"([^"]+)\\\"/',$res['body'],$matches);
         // print_r($matches);exit;
-        $csrftoken = $matches[1];
+        $csrftoken = $matches[1][0];
         $matches = [];
         preg_match_all('/"device_id":"([^$"]+)"/',$res['body'],$matches);
 
-        $device_id = $matches[1];
-        print_r($csrftoken);
-        print_r($device_id);
-        exit;
-        https://www.instagram.com/api/v1/users/web_profile_info/?username=gongdaeyeoja
+        $device_id = $matches[1][0];
+
+        $mid = 'Y5_'.substr(md5($username.date('Ymdh')),0,25);
+
+
+
+
+        // print_r($csrftoken);
+        // print_r($device_id);
+        // exit;
+        $cookies = array();
+        $cookies['csrftoken'] = $csrftoken;
+        $cookies['mid'] =  $mid;
+        $cookies['ig_did'] = $device_id;
+        $cookies['ig_nrcb'] = 1;
+        $cookie = http_build_query($cookies,'','; ',PHP_QUERY_RFC3986);
+        // print_r($cookie);
+
+        $headers = array();
         
-        set-cookie: csrftoken=nzbq1hURr3S55Lzoqd955i61KBvH8rvn; Domain=.instagram.com; expires=Thu, 14-Dec-2023 08:29:47 GMT; Max-Age=31449600; Path=/; Secure
-        set-cookie: mid=Y5ra-gALAAEKcZJnQPbTktcTwHAN; Domain=.instagram.com; expires=Sat, 14-Dec-2024 08:29:47 GMT; Max-Age=63072000; Path=/; Secure
-        strict-transport-security: max-age=31536000
+        $headers[]= "authority: www.instagram.com";
+        $headers[]= "accept: */*";
+        $headers[]= "accept-language: ko-KR,ko;q=0.9";
+        $headers[]= "cache-control: no-cache";
+        $headers[] = 'cookie: '.$cookie;
+        // $headers[]= "cookie: csrftoken=yXEF8fI5r4jQ5HuXQesB58zsX9gwpuEb; mid=Y5_CbwALAAFYtBcEqIS9jlYfnq_X; ig_did=146A211B-0941-4D92-891E-857AF09EF357; ig_nrcb=1";
+        $headers[]= "pragma: no-cache";
+        $headers[]= "referer: https://www.instagram.com/gongdaeyeoja/";
+        $headers[]= "sec-ch-prefers-color-scheme: light";
+        $headers[]= 'sec-ch-ua: "Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"';
+        $headers[]= "sec-ch-ua-mobile: ?0";
+        $headers[]= 'sec-ch-ua-platform: "Windows"';
+        $headers[]= "sec-fetch-dest: empty";
+        $headers[]= "sec-fetch-mode: cors";
+        $headers[]= "sec-fetch-site: same-origin";
+        $headers[]= "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36";
+        $headers[]= "viewport-width: 923";
+        $headers[]= "x-asbd-id: 198387";
+        $headers[]= "x-csrftoken: {$csrftoken}";
+        $headers[]= "x-ig-app-id: 936619743392459";
+        $headers[]= "x-ig-www-claim: 0";
+        $headers[]= "x-requested-with: XMLHttpRequest";
 
-
-        var_dump($res);
+        $web_profile_info_url = "https://www.instagram.com/api/v1/users/web_profile_info/?username={$username}";
+        // var_dump($res['header']);
+        $cacheKey = "web_profile_info_{$username}.json";
+        $res = $this->curlGetResWithCache($web_profile_info_url,$headers,$cacheKey,86400);
+        // print_r($res['body']);
+        $profile_info = json_decode($res['body'],1);
+        $summary_profile_info = $this->summaryProfileInfo($profile_info);
+        // print_r($profile_info);
+        var_dump($summary_profile_info);
         // https://www.instagram.com/api/v1/users/web_profile_info/?username=gongdaeyeoja
 
     }
     
+    private function summaryProfileInfo(& $profile_info){
+
+        $summ = array();
+        foreach($profile_info['data']['user'] as $k => $v){
+            if(is_array($v)){ continue;}
+            $summ[$k] = $v;
+        }
+        $summ['edge_followed_by_count'] = $profile_info['data']['user']['edge_followed_by']['count'];
+        $summ['edge_follow_count'] = $profile_info['data']['user']['edge_follow']['count'];
+        return $summ;
+    }
 
     // og 정보 추려내기
     private function parseOpenGraph($xml){
@@ -156,7 +207,7 @@ class CrawlerInstagram{
         }
         return $body;
     }
-    public function curlGetResWithCache($url,$cacheKey,$expire=3600){
+    public function curlGetResWithCache($url,$headers,$cacheKey,$expire=3600){
         if(!$url[0]){return null;}
         $cached_path = "{$this->cache_dir}/{$cacheKey}";
         $res = null;
@@ -168,7 +219,7 @@ class CrawlerInstagram{
                 echo "expired\n";
             }
             // $url = 'https://www.instagram.com/p/'.urlencode($shortcode).'/';
-            $res = $this->curlGet($url,true);
+            $res = $this->curlGet($url,$headers,true);
             if($this->debug) echo "no-cache\n";
             file_put_contents($cached_path,json_encode($res));
             if($this->debug) echo "save-cache\n";
@@ -177,7 +228,7 @@ class CrawlerInstagram{
     }
 
 
-    public function curlGet($url,$curlopt_header = false){
+    public function curlGet($url,$headers=array(),$curlopt_header = false){
         
         // $url = 'https://www.instagram.com/p/CgZKgZOLsOk/';
 
@@ -186,7 +237,7 @@ class CrawlerInstagram{
 
         // $url = "https://blog.naver.com/WidgetListAsync.naver?blogId={$id}&listNumVisitor=5&isVisitorOpen=true&isBuddyOpen=true&selectCategoryNo=&skinId=0&skinType=C&isCategoryOpen=true&isEnglish=false&listNumComment=5&areaCode=11B10101&weatherType=0&currencySign=ALL&enableWidgetKeys=title%2Cmenu%2Cprofile%2Ccategory%2Ctag%2Ccalendar%2Cmapview%2Csearch%2Clibrary%2Ccomment%2Cvisitor%2Cbuddy%2Ccounter%2Cstat%2Crss%2Cpowered%2Ccontent%2Cgnb%2Cexternalwidget&writingMaterialListType=1&calType=";
 
-        $headers = array(
+        $headers = array_merge(array(
             'Authority: www.instagram.com',
             'Accept: */*',
             'Accept-Language: ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
@@ -201,7 +252,7 @@ class CrawlerInstagram{
             'Sec-Fetch-Site: same-origin',
             // 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
             'User-Agent: cr-instagram',
-        );
+        ),$headers);
 
 
         $conn = curl_init($url);
--- crawler/instagram/test.CrawlerInstagramWebUserInfo.php
+++ crawler/instagram/test.CrawlerInstagramWebUserInfo.php
@@ -8,4 +8,4 @@ $cri->exec_timeout = 10;
 $username = 'gongdaeyeoja';
 $data = $cri->webProfileInfoByUsername($username); /// 만드는 중
 // print_r($mediaData);
-print_r(json_encode($mediaData,JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+// print_r(json_encode($mediaData,JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
