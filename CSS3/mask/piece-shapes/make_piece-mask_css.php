1452b5a5149d543da08e6ef695e359b608491b33
mins01 <mins01.lycos.co.kr@gmail.com>
2023-07-20 16:38:23 +0900

    css mask piece-mask.svg

--- CSS3/mask/piece-shapes/make_piece-mask_css.php
+++ CSS3/mask/piece-shapes/make_piece-mask_css.php
@@ -4,6 +4,10 @@ $arr_right = ['n','0','p'];
 $arr_bottom = ['n','0','p'];
 $arr_left = ['n','0','p'];
 
+
+
+echo '@charset "utf-8";'."\n";
+echo '.piece-shape.mask{ mask-size: contain; -webkit-mask-size: contain; }'."\n";
 foreach($arr_top as $top_k => $top){
     $sets = array();
     foreach($arr_right as $right_k => $right){
@@ -11,7 +15,7 @@ foreach($arr_top as $top_k => $top){
             foreach($arr_left as $left_k => $left){
                 $f_tail = "{$top}{$right}{$bottom}{$left}";
                 $filename = "piece-mask-{$f_tail}.svg";
-                $css = ".piece-shape[data-shape-top=\"{$top}\"][data-shape-right=\"{$right}\"][data-shape-bottom=\"{$bottom}\"][data-shape-left=\"{$left}\"]{mask: url({$filename});-webkit-mask: url({$filename});}";
+                $css = ".piece-shape[data-shape-top=\"{$top}\"][data-shape-right=\"{$right}\"][data-shape-bottom=\"{$bottom}\"][data-shape-left=\"{$left}\"]{mask-image: url({$filename});-webkit-mask-image: url({$filename});}";
                 echo $css."\n";
             }
         }
