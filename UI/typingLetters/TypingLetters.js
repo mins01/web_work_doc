ede20a3938f0c3e47d409c0ab2383512e6ec080b
mins01 <mins01.lycos.co.kr@gmail.com>
2024-03-06 14:45:26 +0900

    TypingLetters

--- UI/typingLetters/TypingLetters.js
+++ UI/typingLetters/TypingLetters.js
@@ -1,48 +1,88 @@
 class TypingLetters{
-    divTypingLetters = null;
+    container = null;
     letters = null;
     lettersIdx = 0;
     typings = null;
     intervalMs = 500;
     tm = null;
     running = false
-    constructor(divTypingLetters){
-        this.divTypingLetters = divTypingLetters;
+    debug = false;
+    constructor(container){
+        this.container = container;
         this.letters = [];
         this.lettersIdx = 0;
         this.intervalMs = 500;
         this.tm = null;
         this.running = false
+        this.debug = false;
     }
-
+    stringToLetters(str){
+        return Array.from(str.replace(/(\r\n|\r)/g,'\n'));
+    }
+    setIntervalMs(intervalMs){
+        this.intervalMs = intervalMs;
+        this.container.style.setProperty('--animation-duration',intervalMs+'ms');
+    }
     init(){
+        this.stop();
         this.lettersIdx = 0;
         this.clear()
     }
     start(){
-        if(this.running){ console.log('이미 실행중입니다.'); return false;}
+        if(this.running){ console.warn('이미 실행중입니다.'); return false;}
         this.running = true;
-        this.tm = setInterval(()=>{
-            if(!this.draw()){
-                this.stop();
-            }
-        },this.intervalMs)
+        if(!this.draw()){
+            this.stop();
+            this.onend();
+        }else{
+            this.tm = setInterval(()=>{
+                if(!this.draw()){
+                    this.stop();
+                    this.onend();
+                }
+            },this.intervalMs)
+        }
+        
+        this.onstart()
+    }
+    onstart(){
+        
     }
     stop(){
+        if(!this.running) return false;
         this.running = false;
         if(this.tm){ clearInterval(this.tm); }
         this.tm = null;
+        this.onstop();
+    }
+    onstop(){
+
+    }
+    onend(){
+
     }
     draw(){
         let letter = this.letters[this.lettersIdx];
         if(letter=== undefined){
             return false;
         }
-        console.log('draw',letter);
-        let divLetter = this.generateDivLetter(letter)
-        this.divTypingLetters.appendChild(divLetter)
+        
+        if(letter==="\n"){
+            if(this.debug) console.log('draw','<br>');
+            let brLetter = this.generateBrLetter()
+            this.container.appendChild(brLetter);
+        }else{
+            if(this.debug)  console.log('draw',letter);
+            let divLetter = this.generateDivLetter(letter)
+            this.container.appendChild(divLetter)
+        }
+        
         this.lettersIdx++;
+        this.ondraw(letter);
         return true;
+    }
+    ondraw(){
+
     }
     generateDivLetter(letter){
         let div = document.createElement('div');
@@ -50,7 +90,16 @@ class TypingLetters{
         div.textContent = letter;
         return div;
     }
+    generateBrLetter(letter){
+        let div = document.createElement('br');
+        div.classList.add('letter');
+        return div;
+    }
     clear(){
-        this.divTypingLetters.innerHTML = '';
+        this.container.innerHTML = '';
+        this.onclear();
+    }
+    onclear(){
+
     }
 }
