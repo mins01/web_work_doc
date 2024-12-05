129fba4cf50eeaf2ed23928c8af5b9bed886a41c
공대여자 <mins01.lycos.co.kr@gmail.com>
2024-12-06 00:48:49 +0900

    booklet-detail preview

--- CSS/booklet/BookletDetail.js
+++ CSS/booklet/BookletDetail.js
@@ -2,9 +2,10 @@
 class BookletDetail{
 
 
-    constructor(target){
+    constructor(target,preview=null){
         this.running=false;
         this.target=target;
+        this.preview=preview;
         this._zoom = 1;
 
         this.scollLeft0 = 0;
@@ -32,11 +33,16 @@ class BookletDetail{
     set zoom(zoom){
         this._zoom = zoom;
         // this.target.style.zoom = zoom;
-        this.target.querySelector('.booklet-detail-img').style.zoom = zoom;
+        this.target.querySelector('.booklet-detail-view-img').style.zoom = zoom;
+    }
+    scrollTo(x,y){
+        this.target.scrollTo(x,y);
+        this.syncPreview();
     }
     center(){
-        this.target.scrollLeft = this.maxScrollLeft / 2
-        this.target.scrollTop = this.maxScrollTop / 2
+        this.scrollTo(this.maxScrollLeft / 2,this.maxScrollTop / 2)
+        // this.target.scrollLeft = this.maxScrollLeft / 2
+        // this.target.scrollTop = this.maxScrollTop / 2
     }
     addEvent(target){
         target.classList.add('moveable');
@@ -63,13 +69,52 @@ class BookletDetail{
         if(!this.running){ return }
         let x = this.x0 - event.x ;
         let y = this.y0 - event.y ;
-        this.target.scrollLeft = this.scollLeft0 + x;
-        this.target.scrollTop = this.scollTop0 + y;       
+        // this.target.scrollLeft = this.scollLeft0 + x;
+        // this.target.scrollTop = this.scollTop0 + y;
+        this.scrollTo(this.scollLeft0 + x, this.scollTop0 + y)
     }
     onpointerup=(event)=>{
         // console.log('onpointerup');
         this.running = false;
         document.removeEventListener('pointermove',this.onpointermove)
         document.removeEventListener('pointerup',this.onpointerup)
+    }
+
+
+
+    // -- preview
+    syncPreview(){
+        if(!this.preview){return;}
+
+        const ow = this.target.offsetWidth;
+        const oh = this.target.offsetHeight;
+        const sl = this.target.scrollLeft;
+        const st = this.target.scrollTop;
+        const sw = this.target.scrollWidth;
+        const sh = this.target.scrollHeight;
+        const psw = this.preview.scrollWidth;
+        const psh = this.preview.scrollHeight;
+        const pow = this.preview.offsetWidth;
+        const poh = this.preview.offsetHeight;
+
+        const ratioScroll = psw/sw;
+        const ratioSize = psw/sw;
+        const w = ow/sw;
+        const h = oh/sh;
+        const l = sl/sw;
+        const t = st/sh;
+        const lpx = l*pow;
+        const tpx = t*poh;
+
+        let parentElement = this.preview.parentElement;
+        parentElement.style.setProperty('--preview-rect-width',(w*100)+'%');
+        parentElement.style.setProperty('--preview-rect-height',(h*100)+'%');
+        // parentElement.style.setProperty('--preview-rect-left',(l*100)+'%');
+        // parentElement.style.setProperty('--preview-rect-top',(t*100)+'%');
+        parentElement.style.setProperty('--preview-rect-left',lpx+'px');
+        parentElement.style.setProperty('--preview-rect-top',tpx+'px');
+
+        // console.log(ratioScroll,w,h);
+        
     }
 }
